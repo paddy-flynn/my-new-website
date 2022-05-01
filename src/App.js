de206a6d1372c43aa71ffa943baca924de7cca15
Paddy Flynn <pflynn210392@gmail.com>
2022-05-01 20:05:19 +0100

    Addinh website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test1 Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="h-screen bg-zinc-800">
+            <div className="ml-40 pt-10">
+                <h1>Patrick Flynn</h1>
+                <div className="json-like">
+                    <p className="op">&#10100;</p>
+                    <p className="data"><span className="key">"me"</span> <span className="sep">:</span> <span className="value">"Patrick Flynn"</span>,
+                    </p>
+                    <p className="data"><span className="key">"linkedIn"</span> <span className="sep">:</span> <span className="value"><a
+                        href="https://www.linkedin.com/in/patrick-flynn-514a1689?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BqmM%2BiNBBRJKu1QsXW5UAEQ%3D%3D"
+                        title="Patrick Flynn on LinkedIn">"https://www.linkedin.com/in/patrick-flynn"</a></span>,</p>
+                    <p className="data"><span className="key">"github"</span> <span className="sep">:</span> <span className="value"><a
+                        href="https://github.com/paddy-flynn"
+                        title="Patrick Flynn on Github">"https://github.com/paddy-flynn"</a></span></p>
+                    <p className="cp">&#10101;</p>
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App;
