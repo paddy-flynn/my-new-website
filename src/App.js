68032ad80fbbf5a9fc91dd14bb9ac6e926cef3d3
Paddy Flynn <pflynn210392@gmail.com>
2022-05-07 01:48:16 +0100

    Fix tests

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
             className="max-w-md mx-auto bg-zinc-800 text-xs text-neutral-300 shadow-md overflow-hidden md:max-w-full min-h-screen">
             <div className="md:flex flex-col">
                 <div>
-                    <h1 className="pl-8 text-2xl mb-8">Patrick Flynn</h1>
+                    <h1 data-testid="my-name" className="pl-8 text-2xl mb-8">Patrick Flynn</h1>
                 </div>
                 <div className="pl-8">
                     <div className="block mb-2">
@@ -15,13 +15,15 @@ export default function App() {
                         <p className="pl-6"><span className="text-cyan-400 w-100 md:text-base">"linkedIn"</span>
                             <span>:</span> <span className="text-amber-500 md:text-base">
                             <a className="no-underline"
-                               href="https://www.linkedin.com/in/patrick-flynn-514a1689?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BqmM%2BiNBBRJKu1QsXW5UAEQ%3D%3D"
+                               data-testid="linkedin-link"
+                               href="https://www.linkedin.com/in/patrick-flynn-514a1689"
                                title="Patrick Flynn on LinkedIn">"https://www.linkedin.com/in/patrick-flynn"</a>
                     </span>,
                         </p>
                         <p className="pl-6"><span className="text-cyan-400 w-100 md:text-base">"github"</span>
                             <span>:</span> <span className="text-amber-500 md:text-base">
                             <a className="no-underline"
+                               data-testid="github-link"
                                href="https://github.com/paddy-flynn"
                                title="Patrick Flynn on Github">"https://github.com/paddy-flynn"</a>
                     </span>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('rendering App component', () => {
+    it('contains my name', () => {
+        render(<App/>);
+        const h1Element = screen.getByTestId('my-name')
+        expect(h1Element).toBeInTheDocument()
+    })
+
+    it('contains a link to linkedIn', () => {
+        render(<App/>);
+        const linkedInElement = screen.getByTestId('linkedin-link')
+        expect(linkedInElement).toBeInTheDocument()
+        expect(linkedInElement).toHaveAttribute('href', 'https://www.linkedin.com/in/patrick-flynn-514a1689')
+    })
+
+    it('contains a link to github', () => {
+        render(<App/>);
+        const linkedInElement = screen.getByTestId('github-link')
+        expect(linkedInElement).toBeInTheDocument()
+        expect(linkedInElement).toHaveAttribute('href', 'https://github.com/paddy-flynn')
+    })
 });
